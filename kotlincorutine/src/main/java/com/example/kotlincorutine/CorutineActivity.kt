b52bdbddc3a17191f3b54e30ef9d28ed607a0e2f
Kmlesh Patel <pkamlesh0124@gmail.com>
2023-02-20 18:56:50 +0530

    Created corutine module

---
+++ kotlincorutine/src/main/java/com/example/kotlincorutine/CorutineActivity.kt
@@ -0,0 +1,11 @@
+package com.example.kotlincorutine
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class CorutineActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_corutine)
+    }
+}
